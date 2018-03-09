66a349f8ff1425743c89ff8aa95898f0361a8af1
Katy Madier <me@katymadier.com>
2018-03-08 21:41:32 -0500

    update

--- chi77/chi77/routers.py
+++
@@ -1,8 +0,0 @@
-
-from rest_framework import routers
-
-from getdata.views import PopulationViewSet
-
-
-router = routers.DefaultRouter()
-router.register('population', PopulationViewSet)
--- chi77/getdata/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib.gis import admin
-from .models import Population
-
-admin.site.register(Population)
