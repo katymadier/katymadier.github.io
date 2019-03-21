5ceec256fe50aa735512037dc79e5cd95e3efe4d
Katy Madier <me@katymadier.com>
2019-03-21 01:15:47 -0400

    update

--- schoolwork/SI659/assignment2/js/daydream-component.js
+++ schoolwork/SI659/assignment2/js/daydream-component.js
@@ -109,26 +109,6 @@ AFRAME.registerComponent('collider-check', {
   }
 });
 
-AFRAME.registerComponent('raycaster-listen', {
-	init: function () {
-    // Use events to figure out what raycaster is listening so we don't have to
-    // hardcode the raycaster.
-    this.el.addEventListener('raycaster-intersected', evt => {
-      this.raycaster = evt.detail.el;
-    });
-    this.el.addEventListener('raycaster-intersected-cleared', evt => {
-      this.raycaster = null;
-    });
-  },
-
-  tick: function () {
-    if (!this.raycaster) { return; }  // Not intersecting.
-
-    let intersection = this.raycaster.components.raycaster.getIntersection(this.el);
-    if (!intersection) { return; }
-    console.log(intersection.point);
-  }
-});
 
 //
 //
